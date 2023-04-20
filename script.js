4be3a40172b1c42f7b20fe9a259edf0a039a7a36
Pedro Vinicius <pedro_vinicius2004@hotmail.com>
2023-04-20 14:04:54 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#myElement").animate({
+        left: '250px',
+        opacity: '0.5',
+        height: '+=150px',
+        width: '+=150px'
+    }, 3000);
+});
